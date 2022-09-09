7d6c63f50c6a60eb3121790bbb76f96fdbd25965
anser <anser7ali@gmail.com>
2022-09-09 12:55:38 +0530

    manual build

--- index.ts
+++ index.ts
@@ -37,7 +37,7 @@ new CronJob(
 app.use("/api", dataSourceRoutes);
 
 app.get("/*", (req, res) => {
-  return res.send({ status: "failure", message: "404 manual!!!" });
+  return res.send({ status: "failure", message: "404 reverted permission!!!" });
 });
 console.log(environment);
 let server = app.listen(PORT, function () {
